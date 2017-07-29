52e6c18a54c85a3df5fb4be0d4821efe844a034c
e <1172597655@qq.com>
2017-07-29 22:20:01 +0800

    second time

---
+++ src/vuex/actions.js
@@ -0,0 +1,14 @@
+import * as types from './mutation-type';
+
+export default {
+  add({
+    commit
+  }) {
+    commit(types.INCREMENT);
+  },
+  dec({
+    commit
+  }) {
+    commit(types.DECMENT);
+  }
+};
